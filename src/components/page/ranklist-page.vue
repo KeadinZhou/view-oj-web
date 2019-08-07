d756d8a2af30c2e758d9042f0aef719ced24820e
Keadin Zhou <34813154+KeadinZhou@users.noreply.github.com>
2019-08-07 20:04:13 +0800

    Update: show the color of rating in ranklist-page.

--- src/components/page/ranklist-page.vue
+++ src/components/page/ranklist-page.vue
@@ -3,38 +3,42 @@
         <el-card shadow="hover" class="tableBox">
             <div v-loading="loading" v-if="isRefresh">
                 <el-table :data="tableData" style="width: 100%">
-                    <el-table-column label="Rank" align="center" width="60px">
+                    <el-table-column label="Rank" align="center" width="100px">
                         <template slot-scope="scope">
                             {{ scope.$index + 1 }}
                         </template>
                     </el-table-column>
                     <el-table-column label="Userid" align="center">
                         <template slot-scope="scope">
-                            {{ scope.row.username }}
+                            <el-link :href="'#/user/'+scope.row.username+'?part=page-top'" :underline="false">
+                                {{ scope.row.username }}
+                            </el-link>
                         </template>
                     </el-table-column>
                     <el-table-column label="Username" align="center">
                         <template slot-scope="scope">
-                            {{ scope.row.nickname }}
+                            <el-link :href="'#/user/'+scope.row.username+'?part=page-top'" :underline="false">
+                                <b :style="'font-weight: 900;color:'+getColorForRating(scope.row.rating)">{{ scope.row.nickname }}</b>
+                            </el-link>
                         </template>
                     </el-table-column>
-                    <el-table-column label="Rating" align="center">
+                    <el-table-column label="Rating" align="center" width="150px">
                         <template slot-scope="scope">
-                            {{ scope.row.rating }}
-                            <el-tooltip effect="dark" :content="'Refresh rating for '+scope.row.nickname" placement="right">
-                                <span style="cursor: pointer;margin-left: 5px" @click="$store.commit('refreshUserRating',scope.row.username)"><i class="el-icon-refresh"></i></span>
-                            </el-tooltip>
+                            <el-link :href="'#/user/'+scope.row.username+'?part=rating'" :underline="false">{{ scope.row.rating }}</el-link>
+<!--                            <el-tooltip effect="dark" :content="'Refresh rating for '+scope.row.nickname" placement="right">-->
+<!--                                <span style="cursor: pointer;margin-left: 5px" @click="$store.commit('refreshUserRating',scope.row.username)"><i class="el-icon-refresh"></i></span>-->
+<!--                            </el-tooltip>-->
                         </template>
                     </el-table-column>
-                    <el-table-column label="Detail" width="70px">
-                        <template slot-scope="scope">
-                            <el-button
-                                    icon="el-icon-s-data"
-                                    size="mini"
-                                    @click="gotoDetail(scope.row)">
-                            </el-button>
-                        </template>
-                    </el-table-column>
+<!--                    <el-table-column label="Detail" width="70px">-->
+<!--                        <template slot-scope="scope">-->
+<!--                            <el-button-->
+<!--                                    icon="el-icon-s-data"-->
+<!--                                    size="mini"-->
+<!--                                    @click="gotoDetail(scope.row)">-->
+<!--                            </el-button>-->
+<!--                        </template>-->
+<!--                    </el-table-column>-->
                 </el-table>
             </div>
         </el-card>
@@ -52,6 +56,17 @@ export default {
     }
   },
   methods: {
+    getColorForRating (rating) {
+      if (rating <= 1600) return '#808080'
+      if (rating <= 1800) return '#008000'
+      if (rating <= 2000) return '#03a89e'
+      if (rating <= 2300) return '#0000ff'
+      if (rating <= 2500) return '#aa00aa'
+      if (rating <= 2800) return '#FF8C00'
+      if (rating <= 3000) return '#ff7777'
+      if (rating <= 3300) return '#ff0000'
+      return '#aa0000'
+    },
     gotoDetail (row) {
       this.$router.push('/user/' + row.username + '?part=rating')
     },
