3573316c1ddebc52ba49421569479a1bebb67127
Keadin Zhou <34813154+KeadinZhou@users.noreply.github.com>
2020-06-12 16:05:40 +0800

    * router/index.js change VIEW into Asynchronous components. * package.json npm update.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import IndexPage from '@/components/page/index-page'
-import AdminPage from '@/components/page/admin-page'
-import LoginPage from '@/components/page/login-page'
-import RanklistPage from '@/components/page/ranklist-page'
-import UserinfoPage from '@/components/page/userinfo-page'
-import MonitorPage from '@/components/page/monitor-page'
-import Error404Page from '@/components/page/error404-page'
-import Error401Page from '@/components/page/error401-page'
 
 Vue.use(Router)
 
@@ -16,56 +8,56 @@ export default new Router({
     {
       path: '/',
       name: 'index-page',
-      component: IndexPage,
+      component: () => import('@/components/page/index-page'),
       meta: {
         title: 'Home - viewOJ'
       }
     }, {
       path: '/admin',
       name: 'admin-page',
-      component: AdminPage,
+      component: () => import('@/components/page/admin-page'),
       meta: {
         title: 'Admin - viewOJ'
       }
     }, {
       path: '/login',
       name: 'login-page',
-      component: LoginPage,
+      component: () => import('@/components/page/login-page'),
       meta: {
         title: 'Login - viewOJ'
       }
     }, {
       path: '/ranklist',
       name: 'ranklist-page',
-      component: RanklistPage,
+      component: () => import('@/components/page/ranklist-page'),
       meta: {
         title: 'Ranklist - viewOJ'
       }
     }, {
       path: '/user/:userid',
       name: 'userinfo-page',
-      component: UserinfoPage,
+      component: () => import('@/components/page/userinfo-page'),
       meta: {
         title: 'Userinfo - viewOJ'
       }
     }, {
       path: '/monitor',
       name: 'monitor-page',
-      component: MonitorPage,
+      component: () => import('@/components/page/monitor-page'),
       meta: {
         title: 'Monitor - viewOJ'
       }
     }, {
       path: '/error401',
       name: 'error401-page',
-      component: Error401Page,
+      component: () => import('@/components/page/error401-page'),
       meta: {
         title: 'Error - viewOJ'
       }
     }, {
       path: '*',
       name: 'error404-page',
-      component: Error404Page,
+      component: () => import('@/components/page/error404-page'),
       meta: {
         title: 'Error - viewOJ'
       }
