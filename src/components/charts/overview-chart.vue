22bd85e71c6e6df12bf625ec98826edb57da8a01
Jiubei0408 <757795210@qq.com>
2020-10-27 23:14:28 +0800

    新生特判，有点丑，回头再说

--- src/components/charts/overview-chart.vue
+++ src/components/charts/overview-chart.vue
@@ -1,135 +1,137 @@
 <template>
-    <div class="chart-box">
-        <div class="chartTitle">
-            <b>Training status chart of Grade {{this.grade}}</b>
-        </div>
-        <div class="chartSubTitle">
-            {{this.daterange && this.daterange[0] ? ('The data from ' + this.daterange[0] + ' to ' + this.daterange[1])
-            : ('Last week\'s data')}}
-        </div>
-        <ve-histogram
-                :data="chartData"
-                :loading="loading"
-                :extend="extend"
-                :legend-visible="false"
-                :mark-line="markLine"
-                :events="chartEvents"
-                width="100%"
-                height="800px">
-        </ve-histogram>
+  <div class="chart-box">
+    <div class="chartTitle">
+      <b>Training status chart of Grade {{ this.grade }}</b>
     </div>
+    <div class="chartSubTitle">
+      {{
+        this.daterange && this.daterange[0] ? ('The data from ' + this.daterange[0] + ' to ' + this.daterange[1])
+            : ('Last week\'s data')
+      }}
+    </div>
+    <ve-histogram
+        :data="chartData"
+        :loading="loading"
+        :extend="extend"
+        :legend-visible="false"
+        :mark-line="markLine"
+        :events="chartEvents"
+        width="100%"
+        height="800px">
+    </ve-histogram>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'overview-chart',
-        props: {
-            grade: String,
-            daterange: Array
-        },
-        data() {
-            var that = this
-            this.markLine = {
-                data: [
-                    {
-                        name: 'AVG',
-                        type: 'average',
-                        label: {
-                            show: true,
-                            position: 'start',
-                            formatter: '{b}\n{c}'
-                        }
-                    }, {
-                        name: 'MAX',
-                        type: 'max',
-                        label: {
-                            show: true,
-                            position: 'start',
-                            formatter: '{b}\n{c}'
-                        }
-                    }, {
-                        name: 'MIN',
-                        type: 'min',
-                        label: {
-                            show: true,
-                            position: 'start',
-                            formatter: '{b}\n{c}'
-                        }
-                    }
-                ]
-            }
-            this.extend = {
-                series: {
-                    label: {show: true, position: 'top'},
-                    barMaxWidth: 70,
-                    itemStyle: {
-                        normal: {
-                            color: '#409eff'
-                        }
-                    }
-                },
-                xAxis: {
-                    triggerEvent: true,
-                    axisLabel: {
-                        margin: 15,
-                        rotate: 45
-                    }
-                },
-                yAxis: {
-                    minInterval: 1
-                },
-                triggerEvent: true
-            }
-            this.chartEvents = {
-                click: function (e) {
-                    var name = e.componentType === 'xAxis' ? e.value : e.name
-                    var username = name.substring(0, name.indexOf(' '))
-                    if (!username) return
-                    that.$router.push('/user/' + username + '?part=problem')
-                }
-            }
-            return {
-                chartData: {
-                    columns: ['User', 'Accept'],
-                    rows: []
-                },
-                loading: false
-            }
-        },
-        methods: {
-            getData() {
-                this.loading = true
-                this.chartData.rows = []
-                for (var i in this.$store.state.OverviewData) {
-                    var item = this.$store.state.OverviewData[i]
-                    if (item.group === this.grade) {
-                        this.chartData.rows.push({'User': item.userid + ' - ' + item.username, 'Accept': item.number})
-                    }
-                }
-                this.loading = false
-            }
-        },
-        created() {
-            this.getData()
+export default {
+  name: 'overview-chart',
+  props: {
+    grade: String,
+    daterange: Array
+  },
+  data() {
+    var that = this
+    this.markLine = {
+      data: [
+        {
+          name: 'AVG',
+          type: 'average',
+          label: {
+            show: true,
+            position: 'start',
+            formatter: '{b}\n{c}'
+          }
+        }, {
+          name: 'MAX',
+          type: 'max',
+          label: {
+            show: true,
+            position: 'start',
+            formatter: '{b}\n{c}'
+          }
+        }, {
+          name: 'MIN',
+          type: 'min',
+          label: {
+            show: true,
+            position: 'start',
+            formatter: '{b}\n{c}'
+          }
         }
+      ]
     }
+    this.extend = {
+      series: {
+        label: {show: true, position: 'top'},
+        barMaxWidth: 70,
+        itemStyle: {
+          normal: {
+            color: '#409eff'
+          }
+        }
+      },
+      xAxis: {
+        triggerEvent: true,
+        axisLabel: {
+          margin: 15,
+          rotate: 45
+        }
+      },
+      yAxis: {
+        minInterval: 1
+      },
+      triggerEvent: true
+    }
+    this.chartEvents = {
+      click: function (e) {
+        var name = e.componentType === 'xAxis' ? e.value : e.name
+        var username = name.substring(0, name.indexOf(' '))
+        if (!username) return
+        that.$router.push('/user/' + username + '?part=problem')
+      }
+    }
+    return {
+      chartData: {
+        columns: ['User', 'Accept'],
+        rows: []
+      },
+      loading: false
+    }
+  },
+  methods: {
+    getData() {
+      this.loading = true
+      this.chartData.rows = []
+      for (var i in this.$store.state.OverviewData) {
+        var item = this.$store.state.OverviewData[i]
+        if (item.group === this.grade) {
+          this.chartData.rows.push({'User': item.userid + ' - ' + item.username, 'Accept': item.number})
+        }
+      }
+      this.loading = false
+    }
+  },
+  created() {
+    this.getData()
+  }
+}
 </script>
 
 <style scoped>
-    .chart-box {
-        margin-top: 50px;
-    }
+.chart-box {
+  margin-top: 50px;
+}
 
-    .chartTitle {
-        text-align: center;
-        font-size: 25px;
-        margin-top: 30px;
-    }
+.chartTitle {
+  text-align: center;
+  font-size: 25px;
+  margin-top: 30px;
+}
 
-    .chartSubTitle {
-        text-align: center;
-        margin-top: 5px;
-        color: #808080;
-        font-size: 13px;
-    }
+.chartSubTitle {
+  text-align: center;
+  margin-top: 5px;
+  color: #808080;
+  font-size: 13px;
+}
 </style>
--- src/components/page/index-page.vue
+++ src/components/page/index-page.vue
@@ -1,117 +1,136 @@
 <template>
-    <div>
-        <div class="dataBox">
-            <el-date-picker
-                    v-model="inputDate"
-                    type="daterange"
-                    align="center"
-                    unlink-panels
-                    value-format="yyyy-MM-dd"
-                    range-separator="to"
-                    start-placeholder="Begin Date"
-                    end-placeholder="End Date"
-                    :picker-options="pickerOptions"
-                    @change="changeOverviewDate">
-            </el-date-picker>
-        </div>
-        <template v-if="isRefresh">
-            <overview-chart v-for="item in this.$store.state.OverviewGrade" :grade="item" :daterange="inputDate"
-                            :key="item"></overview-chart>
-        </template>
+  <div>
+    <div class="dataBox">
+      <el-date-picker
+          v-model="inputDate"
+          type="daterange"
+          align="center"
+          unlink-panels
+          value-format="yyyy-MM-dd"
+          range-separator="to"
+          start-placeholder="Begin Date"
+          end-placeholder="End Date"
+          :picker-options="pickerOptions"
+          @change="changeOverviewDate">
+      </el-date-picker>
     </div>
+    <el-switch
+        style="left: 50%; transform: translate(-50%, 0); margin-bottom: 10px"
+        v-model="isFreshman"
+        active-text="查看新生"
+        inactive-color="#409EFF"
+        inactive-text="查看实验室"/>
+    <template v-if="isRefresh">
+      <overview-chart
+          v-for="item in filtered_grades"
+          :grade="item"
+          :daterange="inputDate"
+          :key="item"/>
+    </template>
+  </div>
 </template>
 
 <script>
-    import OverviewChart from '@/components/charts/overview-chart'
+import OverviewChart from '@/components/charts/overview-chart'
 
-    export default {
-        name: 'index-page',
-        components: {
-            'overview-chart': OverviewChart
-        },
-        data() {
-            return {
-                isRefresh: true,
-                inputDate: [],
-                pickerOptions: {
-                    shortcuts: [{
-                        text: '最近一周',
-                        onClick(picker) {
-                            const end = new Date()
-                            const start = new Date()
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
-                            picker.$emit('pick', [start, end])
-                        }
-                    }, {
-                        text: '最近两周',
-                        onClick(picker) {
-                            const end = new Date()
-                            const start = new Date()
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 14)
-                            picker.$emit('pick', [start, end])
-                        }
-                    }, {
-                        text: '最近一个月',
-                        onClick(picker) {
-                            const end = new Date()
-                            const start = new Date()
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-                            picker.$emit('pick', [start, end])
-                        }
-                    }, {
-                        text: '最近三个月',
-                        onClick(picker) {
-                            const end = new Date()
-                            const start = new Date()
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
-                            picker.$emit('pick', [start, end])
-                        }
-                    }, {
-                        text: '最近一年',
-                        onClick(picker) {
-                            const end = new Date()
-                            const start = new Date()
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 365)
-                            picker.$emit('pick', [start, end])
-                        }
-                    }, {
-                        text: '最近两年',
-                        onClick(picker) {
-                            const end = new Date()
-                            const start = new Date()
-                            start.setTime(start.getTime() - 3600 * 1000 * 24 * 365 * 2)
-                            picker.$emit('pick', [start, end])
-                        }
-                    }]
-                }
-            }
-        },
-        created() {
-            document.title = "Home - viewOJ"
-            this.changeOverviewDate()
-        },
-        methods: {
-            changeOverviewDate() {
-                this.$store.commit('updateOverview', this.inputDate !== null && this.inputDate.length === 2 ? {
-                    start_date: this.inputDate[0],
-                    end_date: this.inputDate[1]
-                } : null)
-                this.isRefresh = false
-                this.$nextTick(function () {
-                    this.isRefresh = true
-                })
-            }
-        }
+export default {
+  name: 'index-page',
+  components: {
+    'overview-chart': OverviewChart
+  },
+  computed: {
+    filtered_grades() {
+      console.log(this.$store.state.OverviewGrade)
+      return Array.from(this.$store.state.OverviewGrade).filter((a) => {
+        if (this.isFreshman) return a.includes("-")
+        else return !a.includes("-")
+      })
     }
+  },
+  data() {
+    return {
+      isFreshman: false,
+      isRefresh: true,
+      inputDate: [],
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近两周',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 14)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一个月',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近三个月',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近一年',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 365)
+            picker.$emit('pick', [start, end])
+          }
+        }, {
+          text: '最近两年',
+          onClick(picker) {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 365 * 2)
+            picker.$emit('pick', [start, end])
+          }
+        }]
+      }
+    }
+  },
+  created() {
+    document.title = "Home - viewOJ"
+    this.changeOverviewDate()
+  },
+  methods: {
+    changeOverviewDate() {
+      this.$store.commit('updateOverview', this.inputDate !== null && this.inputDate.length === 2 ? {
+        start_date: this.inputDate[0],
+        end_date: this.inputDate[1]
+      } : null)
+      this.isRefresh = false
+      this.$nextTick(function () {
+        this.isRefresh = true
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
-    .dataBox {
-        position: relative;
-        width: 350px;
-        left: 50%;
-        transform: translate(-50%, 0);
-        margin-top: 50px;
-        margin-bottom: 50px;
-    }
+.dataBox {
+  position: relative;
+  width: 350px;
+  left: 50%;
+  transform: translate(-50%, 0);
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
 </style>
